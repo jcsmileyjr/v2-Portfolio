194d056d604310e336fc3dea147db0e4cd4606e2
JC Smiley <jcsmileyjr@gmail.com>
2019-04-27 11:36:09 -0500

    Intro Picture, App initial state, Intro component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Intro from './components/intro'
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      intro:false,
+      project:false,
+      about:false,
+    }
+  }
   render() {
     return (
       <div className="container">
-        <h1>Hello World</h1>
+        <Intro />        
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ portfolio/src/components/intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+export default function Intro(props){
+    return(
+      <div className="introImage">
+        <div className="overlay"></div>
+        <h1>JC Smiley</h1>
+        <h2>Software Developer & Tech Meetup Organizer</h2>        
+      </div>        
+    );
+}
