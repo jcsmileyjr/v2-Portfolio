7307d9da7c3602a9eda0a373b9714623ae3fa5b9
JC Smiley <jcsmileyjr@gmail.com>
2019-05-04 11:37:07 -0500

    WIP

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
 import './App.css';
 
 import Intro from './components/intro';
 import Project from './components/project'
 import About from './components/about';
+import Nav from './components/Nav';
+
+function HomePage(props){
+  return(
+    <div>
+      <Nav />
+      <Intro />
+    </div>
+  );
+}
 
 class App extends Component {
   constructor(props){
@@ -17,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="projectContainer">
-          {this.state.intro && <About />}
+          {this.state.intro && <HomePage />}
       </div>
       
     );
--- portfolio/src/components/project.js
+++ portfolio/src/components/project.js
@@ -8,7 +8,7 @@ export default function Project(props){
                 <div><h3>Edge Motors Museum</h3></div>
                 <div><h3>Freelance</h3></div>
             </section>
-            <img class="projectPicture" src={require("../img/intro_meetup.jpg")} alt="Edge Moters Project" />
+            <img class="projectPicture" src={require("../img/intro_meetup_small.jpg")} alt="Edge Moters Project" />
         </div>
     );
 }
