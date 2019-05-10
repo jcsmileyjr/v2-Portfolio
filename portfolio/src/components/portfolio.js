aa0d82a0336180897a47ee88eef17c6475b7175f
JC Smiley <jcsmileyjr@gmail.com>
2019-05-09 22:12:41 -0500

    Added brand photos

--- portfolio/src/components/portfolio.js
+++ portfolio/src/components/portfolio.js
@@ -23,21 +23,21 @@ export default function Portfolio(props){
                                 src={require("../img/car-boy.jpg")} />
                         <Card.Body>
                             <Row>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'react']} size="lg" />
                                     <Card.Text>React Native</Card.Text>
                                 </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'html5']} size="lg" />
+                                    <Card.Text>HTML5</Card.Text>
                                 </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'css3']} size="lg" />
+                                    <Card.Text>CSS3</Card.Text>
                                 </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'js']} size="lg" />
+                                    <Card.Text>JavaScript</Card.Text>
                                 </Col>                                                                                                                                                               
                             </Row>
                             <Card.Subtitle className="rowWhiteSpace">Paid Freelance project completed in April of 2019</Card.Subtitle>                        
@@ -53,24 +53,24 @@ export default function Portfolio(props){
                         <Card.Img 
                                 variant="top"
                                 fluid ={true}                         
-                                src={require("../img/car-boy.jpg")} />
+                                src={require("../img/10mon-c-compressor.png")} />
                         <Card.Body>
                             <Row>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'angular']} size="lg" />
+                                    <Card.Text>AngularJS</Card.Text>
                                 </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'bootstrap']} size="lg" />
+                                    <Card.Text>Bootstrap</Card.Text>
                                 </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'node']} size="lg" />
+                                    <Card.Text>Node/Express</Card.Text>
                                 </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
+                                <Col className="center" xs={6} >
+                                    <FontAwesomeIcon icon={['fab', 'github']} size="lg" />
+                                    <Card.Text>GitHub</Card.Text>
                                 </Col>                                                                                                                                                               
                             </Row>
                             <Card.Subtitle className="rowWhiteSpace">A biggest loser style web app designed specifically for my workplace. The objective is to be the first player to lose 10 pounds.</Card.Subtitle>                        
@@ -86,26 +86,26 @@ export default function Portfolio(props){
                         <Card.Img 
                                 variant="top"
                                 fluid ={true}                         
-                                src={require("../img/car-boy.jpg")} />
+                                src={require("../img/active-case-a-compressor.png")} />
                         <Card.Body>
                             <Row>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
-                                </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
-                                </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
-                                </Col>
-                                <Col>
-                                    <FontAwesomeIcon icon="swimmer" />
-                                    <Card.Text>React Native</Card.Text>
-                                </Col>                                                                                                                                                               
-                            </Row>
+                                <Col className="center" xs={6} >
+                                        <FontAwesomeIcon icon={['fab', 'react']} size="lg" />
+                                        <Card.Text>React</Card.Text>
+                                    </Col>
+                                    <Col className="center" xs={6} >
+                                        <FontAwesomeIcon icon={['fab', 'bootstrap']} size="lg" />
+                                        <Card.Text>Bootstrap</Card.Text>
+                                    </Col>
+                                    <Col className="center" xs={6} >
+                                        <FontAwesomeIcon icon={['fab', 'node']} size="lg" />
+                                        <Card.Text>Node/Express</Card.Text>
+                                    </Col>
+                                    <Col className="center" xs={6} >
+                                        <FontAwesomeIcon icon={['fab', 'css3']} size="lg" />
+                                        <Card.Text>CSS3</Card.Text>
+                                    </Col>
+                                </Row> 
                             <Card.Subtitle className="rowWhiteSpace">A case management app designed to keep the user informed of the current status of each case and allow management of the user's portfolio of cases.</Card.Subtitle>                        
                             
                             <Button variant="link" >View GitHub Code</Button>
