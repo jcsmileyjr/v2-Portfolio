46cd11f32670aab70fd58736bf05976589b879ea
JC Smiley <jcsmileyjr@gmail.com>
2019-05-14 05:43:31 -0500

    add case  studies

--- portfolio/src/caseStudies.js
+++ portfolio/src/caseStudies.js
@@ -84,7 +84,37 @@
             "The best feedback was the request to more legat status for cases. I didn't think through all the ways a case can be lost in the legal process."
         ],
         "results":"The app is currently in use by myself and one other agent."
-    }        
+    },
+    {
+        "pageTitle":"Agent-pedia",
+        "objectives":[
+            "Car photo android mobile app deployed to a kiosk for a vintage car museum located south of downtown Memphis.",
+            "The admin will set each kiosk to display one car, two cars, or a gallery of cars.",
+            "The patrons can view the displayed car, click a photo to view a summary page of the car, or click a button to see additional photos of the selected car."
+        ],
+        "myRole":"I worked on the development team as a software developer along side a UI Designer/Developer. A design firm, Baby Grand, designed the app and passed to us photoshop documents, technical specs, copy, and images.",
+        "techStack":[
+            "HTML, CSS, and JavaScript for app structuring, styling, and functionality",
+            "React Native using Expo for quick scaffolding of the android mobile app file structure, streamline development processes, and building the app’s SDK.",
+            "Android Studio to emulate the kiosk device for testing.",
+            "Microsoft Visual Studio was the team’s code editor of choice.",
+            "Trello to organize tasks from idea to completion.",
+            "Communication between team members was done with Slack.",
+            "Version control with Github as the online repository and GitKraken as the software.",
+            "Photoshop documents was transformed into wire-frames to be developed with Adobe XD."
+        ],
+        "development":[
+            "A component is scaffold using the wireframes and PSD as a guide",
+            "Backend functionality (navigation between pages, updating state, and inserting mock data to be displayed) is then added to the component.",
+            "When completed, the component’s Trello board and Git branch is updated along with a communique via Slack on the progress to the development team.",
+            "The component is then passed on to the UI/UX designer on the team for styling and functionality testing.", 
+            "The component is integrated into the app.",
+            "The app is tested for user experience, functionality, and navigation between pages",
+            "Agile style stand ups every other day",
+            "The final stage of the app was inserting the actual copy and photos to the app, building the mobile app SDK, and testing on the actual device that will be used."
+        ],
+        "results":"The project started and ended in April 2019. We finished the app on time. The app passed the initial stress test of 700 patrons on the grand opening day."
+    }            
 ];
 
 export default caseStudies;
