b2055e386394d4d117cfd96b621293d654798cef
JC Smiley <jcsmileyjr@gmail.com>
2019-05-18 13:13:03 -0500

    Updates

---
+++ precache-manifest.20f45ee04c0d256863a710e341ee6a2a.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "567143dadc9d3fbba85da828478c2332",
+    "url": "/v2-Portfolio/static/media/WOF2.567143da.jpg"
+  },
+  {
+    "revision": "74ed258e37e3d79ee697",
+    "url": "/v2-Portfolio/static/css/main.55ea9777.chunk.css"
+  },
+  {
+    "revision": "e5527dc131d49bf01a33",
+    "url": "/v2-Portfolio/static/js/runtime~main.a49d2a84.js"
+  },
+  {
+    "revision": "d41f628b0467f1f1c5ca3df626c04145",
+    "url": "/v2-Portfolio/static/media/Calculator.d41f628b.gif"
+  },
+  {
+    "revision": "9c652a40352d8f718a2a",
+    "url": "/v2-Portfolio/static/js/2.6a998f0f.chunk.js"
+  },
+  {
+    "revision": "bfe60c5066844b70da06142c78be8feb",
+    "url": "/v2-Portfolio/static/media/meetup-me-standing-small.bfe60c50.jpg"
+  },
+  {
+    "revision": "7990ea04c8b716608359c8f9d45ebe31",
+    "url": "/v2-Portfolio/static/media/intro_meetup.7990ea04.jpg"
+  },
+  {
+    "revision": "4f2dbaf09487606f156e018bec600d33",
+    "url": "/v2-Portfolio/static/media/car-boy.4f2dbaf0.jpg"
+  },
+  {
+    "revision": "74b0b8c6eea233bbf4a85c0644b1e8b8",
+    "url": "/v2-Portfolio/static/media/JC Smiley 2 Page Alpha.74b0b8c6.pdf"
+  },
+  {
+    "revision": "8ccc27079503e8bcca649f1093e57847",
+    "url": "/v2-Portfolio/static/media/lotscars-app.8ccc2707.jpg"
+  },
+  {
+    "revision": "615606b7c2e19b3729393e27d5853c76",
+    "url": "/v2-Portfolio/static/media/twocar-app.615606b7.jpg"
+  },
+  {
+    "revision": "3a4ea4dccbc43363ff4633070972bdd4",
+    "url": "/v2-Portfolio/static/media/redcar-app.3a4ea4dc.jpg"
+  },
+  {
+    "revision": "f3cdce82698b15400bfb457eca81ea6e",
+    "url": "/v2-Portfolio/static/media/car-peoples.f3cdce82.jpg"
+  },
+  {
+    "revision": "aba7b6ff163cb51df395cd85cc2b4b58",
+    "url": "/v2-Portfolio/static/media/active-case-a-compressor.aba7b6ff.png"
+  },
+  {
+    "revision": "b0d827f14efe56a82441b6e5bc55b090",
+    "url": "/v2-Portfolio/static/media/active-case2.b0d827f1.png"
+  },
+  {
+    "revision": "a6780927485078f5257a3f111e803430",
+    "url": "/v2-Portfolio/static/media/agent-pedia-version2-main.a6780927.png"
+  },
+  {
+    "revision": "d7cd76554c01b54e127d17a700f55019",
+    "url": "/v2-Portfolio/static/media/agent-wireframe.d7cd7655.png"
+  },
+  {
+    "revision": "ac5b64282af44058841ffb13e7bb45c5",
+    "url": "/v2-Portfolio/static/media/agentpedia-version2-b-compressor.ac5b6428.png"
+  },
+  {
+    "revision": "cf001f69854b3232f4bc9922b25761a3",
+    "url": "/v2-Portfolio/static/media/agent-Pedia mobile-200.cf001f69.png"
+  },
+  {
+    "revision": "fd36bd16070426b211c53c068c2e8dfa",
+    "url": "/v2-Portfolio/static/media/WOF-Wireframe.fd36bd16.PNG"
+  },
+  {
+    "revision": "74ed258e37e3d79ee697",
+    "url": "/v2-Portfolio/static/js/main.81db4194.chunk.js"
+  },
+  {
+    "revision": "a0b47fc5a7455aa21bf0d20d504092fe",
+    "url": "/v2-Portfolio/static/media/WOF3.a0b47fc5.jpg"
+  },
+  {
+    "revision": "60a48925e102bc77d358b74523897172",
+    "url": "/v2-Portfolio/static/media/WOF5.60a48925.jpg"
+  },
+  {
+    "revision": "8cb478a02bb7632e8048adb00da421f4",
+    "url": "/v2-Portfolio/static/media/WOF4.8cb478a0.jpg"
+  },
+  {
+    "revision": "b172e870869e487a5eee2bf0704d112d",
+    "url": "/v2-Portfolio/static/media/calculator1.b172e870.png"
+  },
+  {
+    "revision": "b8f6c404cbb41993593c67f7f89d983b",
+    "url": "/v2-Portfolio/static/media/calculator2.b8f6c404.png"
+  },
+  {
+    "revision": "b29ecaf5d4dc723c8bf40b3f0f325d5c",
+    "url": "/v2-Portfolio/static/media/calculator3.b29ecaf5.png"
+  },
+  {
+    "revision": "f35462e478caa422605bc80a0c8d3022",
+    "url": "/v2-Portfolio/static/media/calculator4.f35462e4.png"
+  },
+  {
+    "revision": "52e23b72933417d614c5eccb0f5123d1",
+    "url": "/v2-Portfolio/static/media/calculator5.52e23b72.png"
+  },
+  {
+    "revision": "07e95474bd41134008216310758d2b56",
+    "url": "/v2-Portfolio/static/media/cc_meetup2.07e95474.jpg"
+  },
+  {
+    "revision": "4348f6e8ae35dd7a317dd76aab2cbf9e",
+    "url": "/v2-Portfolio/static/media/intro_meetup_small.4348f6e8.jpg"
+  },
+  {
+    "revision": "1d3f48cae4c9fedb33e5a2e81c9b2888",
+    "url": "/v2-Portfolio/static/media/hackMemphis.1d3f48ca.jpg"
+  },
+  {
+    "revision": "6b5f26da1e27c73ce3f8cc4d2b7b49a8",
+    "url": "/v2-Portfolio/static/media/smartCityLookOut.6b5f26da.jpg"
+  },
+  {
+    "revision": "9367c9555763aa9241bed3f7ac9d426c",
+    "url": "/v2-Portfolio/static/media/presentation.9367c955.jpg"
+  },
+  {
+    "revision": "5277f498117e3b5cfb2049820e418b31",
+    "url": "/v2-Portfolio/static/media/inaugural-meetup.5277f498.jpg"
+  },
+  {
+    "revision": "8987176c44581d529a73cae8daa990f0",
+    "url": "/v2-Portfolio/static/media/hike1.8987176c.jpg"
+  },
+  {
+    "revision": "56b65a1f365da487721ca5af52d2f15c",
+    "url": "/v2-Portfolio/static/media/Index Cards Landing Page.56b65a1f.png"
+  },
+  {
+    "revision": "1b61db9d2ff4ce34ddc2e0420a0f47a7",
+    "url": "/v2-Portfolio/static/media/wheel-of-fortune.1b61db9d.gif"
+  },
+  {
+    "revision": "9c652a40352d8f718a2a",
+    "url": "/v2-Portfolio/static/css/2.84666c4f.chunk.css"
+  },
+  {
+    "revision": "fff71ee34940a9675c61031ec138d45a",
+    "url": "/v2-Portfolio/index.html"
+  }
+];
